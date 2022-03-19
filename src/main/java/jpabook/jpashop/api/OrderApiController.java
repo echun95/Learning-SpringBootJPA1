2f10a6f79b06921a2cd0365545afa69e701d382b
leeyoungchun <dldydcns123@gmail.com>
2022-03-19 18:46:01 +0900

    OSIV false -> 컨트롤러 단 로직 QueryService로 변경

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -11,6 +11,7 @@ import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import jpabook.jpashop.service.OrderService;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,13 +52,12 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return result;
     }
+
+    private final OrderQueryService orderQueryService;
+
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3(){
-       List<Order> orders = orderRepository.findAllWithItem();
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
-       return result;
+    public List<jpabook.jpashop.service.query.OrderDto> ordersV3(){
+        return orderQueryService.ordersV3();
     }
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> ordersV3_page(
